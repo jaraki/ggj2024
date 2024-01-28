5ee9e5721d2ce8d9bb8c56e851f28d478ba09d96
Jeffrey Araki <jrok96@gmail.com>
2024-01-27 19:10:49 -0800

    Adding game logic for moving between rounds.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,7 +16,7 @@ public class Game : MonoBehaviour {
     public int DialogDuration = 3;
     public AudioSource GameOverSound;
     public Level[] Levels;
-    public const int MinPlayers = 1;
+    public const int MinPlayers = 4;
     public GameState State;
     public int CurrentLevelIndex;
     public PlayerManager PlayerManager;
@@ -28,6 +29,7 @@ public class Game : MonoBehaviour {
     public TMP_Text FillText;
     private double currentCountdown;
     private double originalFontSize;
+    private float overlap;
     // Start is called before the first frame update
     void Start() {
         ResetTimer();
@@ -48,6 +50,13 @@ public class Game : MonoBehaviour {
         currentCountdown = CountdownTime;
     }
 
+    void SpawnDialog(string text) {
+        var go = Instantiate(DialogPrefab, FindAnyObjectByType<Canvas>().transform);
+        var dialog = go.GetComponent<Dialog>();
+        dialog.SetLine(text);
+        Destroy(go, DialogDuration);
+    }
+
     // Update is called once per frame
     void Update() {
         if(PlayerManager.NumPlayers < MinPlayers) {
@@ -59,10 +68,7 @@ public class Game : MonoBehaviour {
             if (currentCountdown > 0) {
                 if(State == GameState.Waiting) {
                     State = GameState.Countdown;
-                    var go = Instantiate(DialogPrefab, FindAnyObjectByType<Canvas>().transform);
-                    var dialog = go.GetComponent<Dialog>();
-                    dialog.SetLine(Levels[CurrentLevelIndex].OpeningLine);
-                    Destroy(go, DialogDuration);
+                    SpawnDialog(Levels[CurrentLevelIndex].OpeningLine);
                 }
                 
                 currentCountdown -= Time.deltaTime;
@@ -77,19 +83,42 @@ public class Game : MonoBehaviour {
                     State = GameState.Started;
                 }
                 if (Timer.value > 0) {
-                    var overlap = Levels[CurrentLevelIndex].FillShape.CalculateOverlap();
+                    overlap = Levels[CurrentLevelIndex].FillShape.CalculateOverlap();
                     FillText.text = $"{overlap * 100.0f:0.0}%";
                     Timer.value -= Time.deltaTime;
                 }
 
                 TimerText.text = Math.Ceiling(Timer.value).ToString();
                 if (Timer.value <= 0) {
+                    if(State == GameState.Started) {
+                        if (overlap >= 0.76) {
+                            SpawnDialog(Levels[CurrentLevelIndex].EndingLines[0]);
+                        } else if (overlap >= 0.51) {
+                            SpawnDialog(Levels[CurrentLevelIndex].EndingLines[1]);
+                        } else if (overlap >= 0.26) {
+                            SpawnDialog(Levels[CurrentLevelIndex].EndingLines[2]);
+                        } else {
+                            SpawnDialog(Levels[CurrentLevelIndex].EndingLines[3]);
+                        }
+                        StartCoroutine(StartNextLevel());
+                    }
                     TimerText.text = "Time's Up!";
                     State = GameState.Ended;
                 }
             }
         }
 
+        IEnumerator StartNextLevel() {
+            yield return new WaitForSeconds(3);
+            SpawnDialog(Levels[CurrentLevelIndex].ClosingLine);
+            yield return new WaitForSeconds(3);
+            
+            State = GameState.Waiting;
+            ResetTimer();
+            ResetCountdown();
+            Levels[CurrentLevelIndex].gameObject.SetActive(false);
+            CurrentLevelIndex++;
+        }
 
     }
 }
